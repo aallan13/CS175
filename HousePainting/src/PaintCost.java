3add930843e7f4a677a63cd44e46f765f27e4f73
Abdul Allan <s1160162@monmouth.edu>
2019-09-16 20:56:52 -0400

    Variables created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,33 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program will calculate the cost to padouble a house 
+		 */
+		
+		double houseLengthFeet;
+		double houseLengthInch;
+		double houseWidthFeet;
+		double houseWidthInch;
+		double houseHeightFeet;
+		double houseHeightInch;
+		double windowLengthfeet;
+		double windowLengthInch;
+		double windowWidthFeet;
+		double windowWidthInch;
+		int windowNumber;
+		double doorLengthFeet;
+		double doorLengthInch;
+		double doorWidthFeet;
+		double doorWidthInch;
+		int doorNumber;
+		double sqFtCost;
+		double sqFtNormal;
+		double sqFtWindows;
+		double sqFtDoors;
+		
+
+	}
+
+}
